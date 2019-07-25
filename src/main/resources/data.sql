3916ed234938d62611a04f92d627d9735fafbd49
manimekalai.k <manimekala8797.com>
2019-07-25 12:59:34 +0530

    application modify

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into car (id,name) values (1,'Honda');
-insert into car (id,name) values (2,'Hyndai');
-insert into car (id,name) values (3,'Suzuki');
-insert into car (id,name) values (4,'Renault');
-insert into car (id,name) values (5,'Audi');
+insert into car (id,name) values (6,'Honda');
+insert into car (id,name) values (7,'Hyndai');
+insert into car (id,name) values (8,'Suzuki');
+insert into car (id,name) values (9,'Renault');
+insert into car (id,name) values (10,'Audi');
